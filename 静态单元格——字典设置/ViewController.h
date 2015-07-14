ed69c0c8436480c3554378658378161043bd76ed
Gate <luchangxiang1@163.com>
2015-07-14 13:24:15 +0800

    Initial Commit

---
+++ 静态单元格——字典设置/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  静态单元格——字典设置
+//
+//  Created by Gate on 15/7/14.
+//  Copyright (c) 2015年 Gate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
